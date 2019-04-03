d6451017a0739e1f7c67027ab3056c49e6ca63e2
Wojciech Mojsiejuk <wojciechmojsiejuk@MacBook-Pro-Wojciech.local>
2019-04-03 11:05:52 +0200

    created command prompt

--- main.c
+++ main.c
@@ -1,3 +1,37 @@
-int main(){
+#include <unistd.h>
+#include <stdio.h>
+void printCommandPrompt()
+{
+  //buffor do przechowywania ścieżki w której obecnie jest użytkownik
+  char cwd[256];
+  //wyświetlenie ścieżki użytkownika
+  if (getcwd(cwd, sizeof(cwd)) == NULL)
+      perror("getcwd() error");
+  else
+      printf("%s#", cwd);
+}
+int readLineFromCommandPrompt()
+{
+  ssize_t read;
+  size_t len = 0;
+  char *line = NULL;
+  read = getline(&line, &len, stdin);
+  if( read == -1)
+    {
+        perror("Unable to allocate buffer");
+        return read;
+    }
+  printf("%s",line);
+  return read;
+
+}
+int main()
+{
+  printCommandPrompt();
+  int user_response = readLineFromCommandPrompt();
+  if( user_response != -1)
+    {
+      printf("Dzieki dziala");
+    }
   return 0;
 }
