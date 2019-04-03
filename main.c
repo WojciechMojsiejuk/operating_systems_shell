10a09391dd7bc4bf8d0995bd83436e50c888925b
Wojciech Mojsiejuk <wojciechmojsiejuk@MacBook-Pro-Wojciech.local>
2019-04-03 11:44:45 +0200

    Split function

--- main.c
+++ main.c
@@ -15,23 +15,41 @@ int readLineFromCommandPrompt()
   ssize_t read;
   size_t len = 0;
   char *line = NULL;
+  int bufsize = 64, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
   read = getline(&line, &len, stdin);
   if( read == -1)
     {
         perror("Unable to allocate buffer");
         return read;
     }
-  printf("%s",line);
+  printf("%s\n",line);
+  token = strtok(line, " \t\r\n\a");
+  while (token != NULL)
+  {
+    tokens[position] = token;
+    position++;
+  }
+  int i=0;
+  for(;i<position;i++)
+  {
+    printf("%s",tokens[i]);
+  }
   return read;
 
 }
 int main()
 {
-  printCommandPrompt();
-  int user_response = readLineFromCommandPrompt();
-  if( user_response != -1)
-    {
-      printf("Dzieki dziala");
-    }
+  while(1)
+  {
+    printCommandPrompt();
+    int user_response = readLineFromCommandPrompt();
+    if( user_response != -1)
+      {
+        printf("Dzieki dziala");
+      }
+
+  }
   return 0;
 }
