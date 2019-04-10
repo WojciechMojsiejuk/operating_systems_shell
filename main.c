776a561531363e713666e8af475b6aba2678eaad
Wojciech Mojsiejuk <wojciechmojsiejuk@MacBook-Pro-Wojciech.local>
2019-04-10 09:35:55 +0200

    added queue to store history

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 void printCommandPrompt()
 {
@@ -130,12 +132,44 @@ void execute(char** command, int tokenCount)
 			return;
 		}
 	}
-  char** bufor = malloc(tokenCount * sizeof(char*));
+  char** first_buffer = malloc(tokenCount * sizeof(char*));
+  char** second_buffer = malloc(tokenCount * sizeof(char*));
+  int is_pipe=0; //by default set this flag to false
+  int is_redirect=0;  //by default set this flag to false
+  int j=0; //first_buffer's index
+  int k=0; //second_buffer's index
   for (i = 0; i < tokenCount; i++)
   {
     if (tokenType[i]==3 || tokenType[i]==4)
     {
-        bufor[i] = command[i];
+      if(is_pipe==0 && is_redirect==0)
+      {
+        first_buffer[j] = command[i];
+        j++
+      }
+      else
+      {
+        second_buffer[k]=command[i];
+      }
+      if(tokenType[i]==1)
+      {
+        if(is_pipe==1)
+        {
+          //wywolaj funkcje bo doszlismy do drugiego pipe'a
+        }
+        is_pipe==1;
+      }
+      if(tokenType[i]==2)
+      {
+        if(is_redirect==2)
+        {
+          //wywolaj funkcje bo doszlismy do drugiego pipe'a
+        }
+        is_redirect==2;
+      }
+      //pipe = 1, redirect = 2, parameter = 3, command = 4, backgroundProcess = 5
+    	//TODO: check type of token (-, >>, | etc.) and do action
+
     }
     //printf("%s",bufor[i]);
 
@@ -159,6 +193,13 @@ void execute(char** command, int tokenCount)
 
 int main()
 {
+  //pobranie historii poleceń
+  const char *homedir;
+  if ((homedir = getenv("HOME")) == NULL)
+  {
+    homedir = getpwuid(getuid())->pw_dir;
+  }
+  printf("HOME: %s \n",homedir);
   while(1)
   {
 	printCommandPrompt();
